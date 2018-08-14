faab5110d2cd8914afb00b221fd97623f58aa595
wisdom <smc6711@theminda.com>
2018-08-14 18:01:17 +0900

    오탈자 수정

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -5,7 +5,7 @@ const connectionOptions: ConnectionOptions = {
     database: "nuber",
     synchronize: true,
     logging: true,
-    entities: ["entities/**/.*"],
+    entities: ["entities/**/*.*"],
     host: process.env.DB_ENDPOINT,
     port: 5432,
     username: process.env.DB_USERNAME,
