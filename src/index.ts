0386cffb64ec0925f5fae51cfae556a722ed2988
wisdom <smc6711@theminda.com>
2018-08-14 15:07:45 +0900

    postgres 접속환경 기본 셋팅

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { Options } from  "graphql-yoga";
-import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT : number | string = process.env.PORT || 4000;
-const PLAYGROUND_ENDPOINT : string = "/playgroud";
-const GRAPHQL_ENDPOINT : string = "/graphql";
+import { Options } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import app from "./app";
+import connectionOptions from "./ormConfig";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
 
 const appOptions: Options = {
-    port: PORT,
-    playground: PLAYGROUND_ENDPOINT,
-    endpoint: GRAPHQL_ENDPOINT
-}
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT
+};
 
-const handleAppStart = () => console.log(`Listening on port ${PORT}`)
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
-app.start(appOptions, handleAppStart);
+createConnection(connectionOptions)
+  .then(() => {
+    app.start(appOptions, handleAppStart);
+  })
+  .catch(error => console.log(error));
