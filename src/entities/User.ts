1895ce63c3befe953318bae6c57bc57fa74906e3
smc0210 <smc0210@naver.com>
2019-10-04 15:19:43 +0900

    폰인증...일단 마무리
    - node 12->10으로 다운그레이드 함

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -46,7 +46,7 @@ class User extends BaseEntity {
     phoneNumber: string;
 
     @Column({ type: "boolean", default: false })
-    verifiedPhonenNumber: boolean;
+    verifiedPhoneNumber: boolean;
 
     @Column({ type: "text" })
     profilePhoto: string;
